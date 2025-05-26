d2d0e7bf8c75bf79835d010970846e5f96dc0f43
harsh <harshp.ug23.cs@nitp.ac.in>
2025-05-26 16:07:59 +0530

    Your commit message here

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import { TodoForm } from './components/TodoForm'
 import styles from './App.module.css'
-
+import { useState } from 'react'
 function App() {
+  const [todos, setTodos] = useState([])  
+  function handleCreate(newTodo) {
+    setTodos((prevTodos) => [...prevTodos, {id: `${prevTodos.length+1}`,...newTodo}]);
+  }
+  
   return (
     <div className={styles.App}>
       <header className={styles.Header}>
@@ -9,9 +14,16 @@ function App() {
         <h2 className={styles.Title}>Todo App</h2>
       </header>
    
-      <div className={styles.AppContainer}><TodoForm/></div>
+      <div className={styles.AppContainer}><TodoForm onCreate={handleCreate}/>
+      {JSON.stringify(todos)}
+      </div>
     </div>
   )
 }
 
 export default App
+
+
+
+
+
--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,18 +1,35 @@
 import styles from "./TodoForm.module.css";
-export function TodoForm() {
+export function TodoForm({onCreate}) { 
+  function handleSubmit(event) {
+    event.preventDefault();
+    const {elements} = event.target
+    if(elements.name.value === "") {
+      alert("Name is required");
+      return;
+    }
+    onCreate({   
+      name: elements.name.value,
+      description: elements.description.value,
+      deadline: elements.deadline.value,
+      priority: elements.priority.value,
+      completed: false,
+    });
+    event.target.reset();
+  }
   return(
     <section>
       <h3 className={styles.Title} >New-To-Do</h3>
-        <form className={styles.Form}>
+        <form className={styles.Form} onSubmit={handleSubmit}>
             <div className={styles.FormFields}>
              <div className={styles.FormField}>
             <input 
-                type="text"
-               aria-label="Name*"
-               placeholder="Name*"
-               name=" name"
-               autoComplete="off"
-               />
+  type="text"
+  aria-label="Name*"
+  placeholder="Name*"
+  name="name" 
+  autoComplete="off"
+/>
+
                </div>
               <div className={styles.FormField}>
             <textarea
@@ -33,7 +50,7 @@ export function TodoForm() {
                 aria-label="Deadline"
                 />
             </div>
-          <label htmlfor="priority">Priority</label>    
+          <label htmlFor="priority">Priority</label>    
             <select defaultValue="none" name="priority" id="priority">
                 <option value="none" disabled>-- Select Priority --</option>
                 <option value="low">Low</option>
